ca54568b9649c25ad84dde12a22ea14cf40e4f77
Benjy <invartam@gmail.com>
2017-02-20 22:57:09 +0100

    First commit

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -n $1 ] ; then
+  XMX='3G'
+  XMS='2G'
+  JAVA_ARGS="-Xmx${XMX} -Xms${XMS}"
+else
+  JAVA_ARGS=$1
+fi;
+
+JAR='Tekkit.jar'
+
+if [ ! -e /minecraft/server.properties ]; then
+  unzip /data/server.zip -d /minecraft
+fi
+cd /minecraft && java $1 -jar $JAR nogui
